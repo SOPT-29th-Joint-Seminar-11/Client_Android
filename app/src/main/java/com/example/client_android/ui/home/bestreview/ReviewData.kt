ad773ead83ff3cd55e5eabd43eb232f3ec6372fa
minwoo <alsd20177@gmail.com>
2021-11-17 03:55:39 +0900

    [feat] #4 - Add recyclerView & item list & data class for best review recyclerView

---
+++ app/src/main/java/com/example/client_android/ui/home/bestreview/ReviewData.kt
@@ -0,0 +1,13 @@
+package com.example.client_android.ui.home.bestreview
+
+data class ReviewData(
+    val cafeName: String,
+    val distance: Int, // 7km
+    val kindAndLocation: String,
+    // rating bar에는 어떻게 넣어줘야 하나?
+    val averageScore: Int,
+    val cafeImage: Int, // imageView에 넣을 데이터는 어떤 형식?
+    val review: String,
+    val timeAge: Int // 46초전
+
+)
