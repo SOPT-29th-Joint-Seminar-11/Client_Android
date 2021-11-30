fa4c3d44aeb3dfa32f9ee36e774d51f6026df8d4
gimangi <khk211113@naver.com>
2021-11-30 20:15:35 +0900

    [FEAT] #16 - Create ResponseReserve, Edit ReserveService

--- app/src/main/java/com/example/client_android/network/service/ReserveService.kt
+++ app/src/main/java/com/example/client_android/network/service/ReserveService.kt
@@ -1,13 +1,15 @@
 package com.example.client_android.network.service
 
+import com.example.client_android.network.model.ResponseReserve
 import retrofit2.Call
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ReserveService {
     @Headers("Content-Type:application/json")
     @POST("cafe-detail/{cafeId}/reserve")
     fun postReserve(
-
+        @Path("cafeId") cafeId: Long
     ) : Call<ResponseReserve>
 }
