838a901aaaccfd2e7da550158b7eab4f88f22339
gimangi <khk211113@naver.com>
2021-12-02 05:12:41 +0900

    [FEAT] #16 - Apply style to dialog

--- app/src/main/java/com/example/client_android/util/DialogUtil.kt
+++ app/src/main/java/com/example/client_android/util/DialogUtil.kt
@@ -2,18 +2,38 @@ package com.example.client_android.util
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import com.example.client_android.R
 
 fun Context.simpleDialog(msg: String, btn: String) {
-    AlertDialog.Builder(this)
-        .setMessage(msg)
-        .setPositiveButton(btn, object: DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface, which: Int) {
 
-            }
-        })
+    val layoutInflater = LayoutInflater.from(this)
+    val view = layoutInflater.inflate(R.layout.dialog_simple, null)
+
+    val alertDialog = AlertDialog.Builder(this)
+        .setView(view)
         .create()
-        .show()
+
+    val tvMessage = view.findViewById<TextView>(R.id.tv_dialog_reserve_msg)
+    val btnConfirm = view.findViewById<Button>(R.id.btn_dialog_reserve_ok)
+
+    tvMessage.text = msg
+    btnConfirm.text = btn
+    btnConfirm.setOnClickListener {
+        alertDialog.dismiss()
+    }
+
+    // radius 적용을 위한 background 적용
+    alertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+    alertDialog.show()
+
 }
 
 object DialogUtil {
